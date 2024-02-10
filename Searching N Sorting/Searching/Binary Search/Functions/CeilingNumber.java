25df2675d8e8b5821df10ed7104fdeabc2f0d2ef
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-02-10 10:40:43 +0530

    CeilingNumber.java file added

---
+++ Searching N Sorting/Searching/Binary Search/Functions/CeilingNumber.java
@@ -0,0 +1,25 @@
+package Problems;
+
+public class CeilingNumber {
+    public static void main(String[] args) {
+        int[] arr = {11,22,33,44,55,66,77,88,99,110};
+        int target = 80;
+        System.out.print(ceilingNumber(arr,target));
+    }
+
+    static int ceilingNumber(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int middle = (start + end) / 2;
+            if (target < arr[middle]) {
+                end = middle - 1;
+            } else if (target > arr[middle]) {
+                start = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return arr[start];
+    }
+}
